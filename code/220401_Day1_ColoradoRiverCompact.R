ef1df8470800a3ff42cd4761fc7de3d31c6a60f9
Adam Price <adamnicholasprice@gmail.com>
2022-04-01 15:51:31 -0700

    inital push and day 1

---
+++ code/220401_Day1_ColoradoRiverCompact.R
@@ -0,0 +1,21 @@
+#####################################################################
+##
+## Script name: 220401_Day1_ColoradoRiverCompact.R
+##
+## Author: Adam N. Price
+##
+## Date Created: 2022-04-01
+##
+## Copyright (c) Adam N. Price, 2022
+## Email: adnprice@ucsc.edu
+##
+############################# Description ##########################
+##
+## Plot of #30DayChartChallenger/Edition2022
+##   
+##
+############################# Packages #############################
+library(tidyverse)
+library(ggplot2)
+############################# Code ################################
+
